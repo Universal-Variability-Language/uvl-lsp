8dce3d2e435588432733f815cfdf1bd2ad8ea42d
Max <caradhras11@gmail.com>
2023-01-30 14:11:41 +0100

    up version

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ mod parse;
 mod query;
 mod semantic;
 mod util;
-static VERSION: &str = "v0.0.2";
+static VERSION: &str = "v0.0.3";
 //The server core, request and respones handling
 struct Backend {
     client: Client,
